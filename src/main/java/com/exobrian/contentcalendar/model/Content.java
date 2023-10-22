e59499e0ccc8d131ad10e8ffec2a1638d4884172
Brian Tran <exobrian@yahoo.com>
2023-10-21 20:02:44 -0700

    https://youtu.be/UgX5lgv4uVM?si=4OsemIII76x8J_tR&t=7762

--- src/main/java/com/exobrian/contentcalendar/model/Content.java
+++ src/main/java/com/exobrian/contentcalendar/model/Content.java
@@ -1,10 +1,15 @@
 package com.exobrian.contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
     Integer id,
+
+    @NotBlank
     String title,
+
     String desc,
     Status status,
     Type contentType,
